f0c16b6c692a4fc438be595769221a7ce4270ed6
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-05-19 02:22:05 +0300

    added everything

---
+++ AABB.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace starikcetin.ManualTracking2D
+{
+    /// <summary>
+    /// Contains info about the AABB of a TrackedObjectData.
+    /// </summary>
+    public class AABB
+    {
+        public Vector2 Center { get; private set; }
+        public Vector2 Extents { get; private set; }
+
+        public Vector2 Max
+        {
+            get { return Center + Extents; }
+        }
+
+        public Vector2 Min
+        {
+            get { return Center - Extents; }
+        }
+
+        public void Update(Bounds bounds)
+        {
+            Center = bounds.center;
+            Extents = bounds.extents;
+        }
+    }
+}
---
+++ ObjectTrackerDatabase.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace starikcetin.ManualTracking2D
+{
+    /// <summary>
+    /// Keeps records of all TrackedObjectDatas and their referances.
+    /// </summary>
+    public static class ObjectTrackerDatabase
+    {
+        #region Fields and Properties
+
+        private static readonly List<TrackedObjectData> AllTrackedObjects = new List<TrackedObjectData>();
+
+        private static readonly Dictionary<GameObject, TrackedObjectData> ObjectDataDictionary =
+            new Dictionary<GameObject, TrackedObjectData>();
+
+        private static readonly Dictionary<TrackedObjectData, GameObject> DataObjectDictionary =
+            new Dictionary<TrackedObjectData, GameObject>();
+
+        /// <summary>
+        /// Gets all datas as read-only. DO NOT try to modify this by any meanings.
+        /// </summary>
+        public static List<TrackedObjectData> AllDatas
+        {
+            get { return AllTrackedObjects; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static void Add(GameObject gameObject, TrackedObjectData data)
+        {
+            AllTrackedObjects.Add(data);
+            ObjectDataDictionary.Add(gameObject, data);
+            DataObjectDictionary.Add(data, gameObject);
+        }
+
+        public static void Remove(GameObject gameObject)
+        {
+            var data = ObjectDataDictionary[gameObject];
+
+            AllTrackedObjects.Remove(data);
+            ObjectDataDictionary.Remove(gameObject);
+            DataObjectDictionary.Remove(data);
+        }
+
+        public static void Remove(TrackedObjectData data)
+        {
+            var gameObject = DataObjectDictionary[data];
+
+            AllTrackedObjects.Remove(data);
+            ObjectDataDictionary.Remove(gameObject);
+            DataObjectDictionary.Remove(data);
+        }
+
+        public static void UpdateAll()
+        {
+            var count = AllTrackedObjects.Count;
+            for (var i = 0; i < count; i++)
+            {
+                var data = AllTrackedObjects[i];
+
+                if (data.GameObject.activeSelf)
+                {
+                    data.Update();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
